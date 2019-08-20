66719d8fb9267076390b32cafb0973a67fb0afaa
rajavijayakumar <developer.rajanayagam@gmail.com>
2019-08-20 18:30:55 +0530

    Added Example files

--- Example/FullCustomLoader/ViewController.swift
+++ Example/FullCustomLoader/ViewController.swift
@@ -1,24 +1,51 @@
 //
 //  ViewController.swift
-//  FullCustomLoader
+//  FullMaterialLoader
 //
-//  Created by monicarajendran on 08/20/2019.
-//  Copyright (c) 2019 monicarajendran. All rights reserved.
+//  Created by vgsathish1995@gmail.com on 02/14/2018.
+//  Copyright (c) 2018 vgsathish1995@gmail.com. All rights reserved.
 //
 
 import UIKit
+import FullCustomLoader
+
 
 class ViewController: UIViewController {
-
+    
+    var indicator: MaterialLoadingIndicator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.configViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func configViews() {
+        
+        indicator = MaterialLoadingIndicator(frame: CGRect(x:0, y:0, width: 30, height: 30))
+        indicator.indicatorColor = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+        indicator.startAnimating()
     }
-
+    
+    // ************ Not Using *****************
+    @IBAction func startLoader(_ sender: Any) {
+        indicator.isHidden = false
+        indicator.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+            
+            self.indicator.isHidden = true
+            self.indicator.stopAnimating()
+        })
+    }
+    
+    
+    @IBAction func stopLoader(_ sender: Any) {
+        indicator.stopAnimating()
+        indicator.isHidden = true
+    }
+    
 }
 
